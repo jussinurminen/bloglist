9ce8820ecf282ecac09d41d2de33b86556af37f9
Jussi Nurminen <jussi.nurminen@tuni.fi>
2020-04-11 22:00:58 +0300

    4.4: apufunktioita ja yksikkötestejä, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,10 +1,14 @@
-const dummy = ( blogs ) => {
-  const reducer = ( sum, item ) => {
-    return sum + item
-  }
+const dummy = ( ) => {
   return 1
 }
-  
+
+const totalLikes = ( blogs ) => {
+  return blogs.reduce(function (sum, currentValue) {
+    return sum + currentValue.likes
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
