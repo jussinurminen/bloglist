db6112a24063547762775df1f498a04b459cde05
Jussi Nurminen <jussi.nurminen@tuni.fi>
2020-04-13 16:07:33 +0300

    Updates

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,17 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs.map(blog => blog.toJSON()))
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.get('/:id', (request, response, next) => {
   Blog.findById(request.params.id)
-    .then(person => {
-      if (person) {
-        response.json(person.toJSON())
+    .then(blog => {
+      if (blog) {
+        response.json(blog.toJSON())
       } else {
         response.status(404).end()
       }
@@ -19,7 +18,7 @@ blogsRouter.get('/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
   const blog = new Blog({
@@ -29,11 +28,12 @@ blogsRouter.post('/', (request, response, next) => {
     likes: body.likes,
   })
 
-  blog.save()
-    .then(savedBlog => {
-      response.json(savedBlog.toJSON())
-    })
-    .catch(error => next(error))
+  try {
+    const savedBlog = await blog.save()
+    response.json(savedBlog.toJSON())
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 blogsRouter.delete('/:id', (request, response, next) => {
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,12 +9,11 @@ const totalLikes = ( blogs ) => {
 }
 
 const favoriteBlog = ( blogs ) => {
-  // palauttaa blogin, jolla eniten tykkäyksiä. Jos näitä monta niin palauttaa jonkun niistä
   const likesMax = Math.max.apply(null, blogs.map(function(o) { return o.likes }))
-  //const maxLikesObject = blogs.filter(function(o) { return o.likes === likesMax })[0]
   const maxLikesObject = blogs.filter(function(o) { return o.likes === likesMax })
   return maxLikesObject
 }
+
 
 module.exports = {
   dummy,
