0f35f3beb75f59ce97d08ff6beed6acc1add0c73
Jussi Nurminen <jussi.nurminen@tuni.fi>
2020-04-13 16:42:05 +0300

    Modifications when using library express-async-error

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,20 +6,17 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.get('/:id', async (request, response, next) => {
-  try{
-    const blog = await Blog.findById(request.params.id)
-    if (blog) {
-      response.json(blog.toJSON())
-    } else {
-      response.status(404).end()
-    }
-  } catch(exception) {
-    next(exception)
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog.toJSON())
+  } else {
+    response.status(404).end()
   }
+
 })
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
   const blog = new Blog({
@@ -29,21 +26,14 @@ blogsRouter.post('/', async (request, response, next) => {
     likes: body.likes,
   })
 
-  try {
-    const savedBlog = await blog.save()
-    response.json(savedBlog.toJSON())
-  } catch(exception) {
-    next(exception)
-  }
+  const savedBlog = await blog.save()
+  response.json(savedBlog.toJSON())
 })
 
-blogsRouter.delete('/:id', async (request, response, next) => {
-  try {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  } catch (exception) {
-    next(exception)
-  }
+blogsRouter.delete('/:id', async (request, response) => {
+
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 blogsRouter.put('/:id', (request, response, next) => {
