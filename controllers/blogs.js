fadee798d942c24a48a2159b11b889d76a5a723e
Jussi Nurminen <jussi.nurminen@tuni.fi>
2020-04-13 16:29:08 +0300

    Async Get and Delete added

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,16 +6,17 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog.toJSON())
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+blogsRouter.get('/:id', async (request, response, next) => {
+  try{
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog.toJSON())
+    } else {
+      response.status(404).end()
+    }
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 blogsRouter.post('/', async (request, response, next) => {
@@ -36,12 +37,13 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
 })
 
 blogsRouter.put('/:id', (request, response, next) => {
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -59,8 +59,8 @@ describe('API tests', () => {
     const blogsAtEnd = await helper.blogsInDb()
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-    const title = blogsAtEnd.map(n => n.title)
-    expect(title).toContain(
+    const titles = blogsAtEnd.map(n => n.title)
+    expect(titles).toContain(
       'TDD harms architecture'
     )
   })
@@ -80,6 +80,38 @@ describe('API tests', () => {
     const blogsAtEnd = await helper.blogsInDb()
   
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specific blog can be viewed', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+  
+    const blogToView = blogsAtStart[0]
+  
+    const resultBlog = await api    
+      .get(`/api/blogs/${blogToView.id}`)    
+      .expect(200)    
+      .expect('Content-Type', /application\/json/)
+      
+    expect(resultBlog.body).toEqual(blogToView)
+  })
+
+  test('a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+  
+    await api    
+      .delete(`/api/blogs/${blogToDelete.id}`)    
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+  
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
+  
+    const titles = blogsAtEnd.map(r => r.title)
+  
+    expect(titles).not.toContain(blogToDelete.title)
   })
 
   afterAll(() => {
