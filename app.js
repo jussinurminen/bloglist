ace60c1f972e1fc6b41f8b39b0f765ef43490684
Jussi Nurminen <jussi.nurminen@tuni.fi>
2020-04-14 17:08:17 +0300

    App.js modified

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -13,6 +14,7 @@ logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     logger.info('connected to MongoDBx')
+    console.log('connected to MongoDBx (app.js)',config.MONGODB_URI)
   })
   .catch((error) => {
     logger.error('error connection to MongoDB:', error.message)
@@ -24,6 +26,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
